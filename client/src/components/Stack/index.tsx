23edcd0b7d8a03ab54c6f815831bbb5593a631e0
KozaX <55134615+KozaX@users.noreply.github.com>
2020-11-02 20:12:40 +0100

    add stack section and stylings to the card

--- client/src/components/Stack/index.tsx
+++ client/src/components/Stack/index.tsx
@@ -1,13 +1,36 @@
 import React from 'react'
 
+//styles
+import './styles.scss'
+
+//components
+import Item from './Item'
+
 const Stack: React.FC = () => {
-  const frontEndLanguages = ['React', 'Bootstrap', 'Rest APIs', 'Redux', 'Sass', 'Handlebars', 'Typescript', 'Styled Components']
   return (
-    <div>
-      <ul className="list-group">
-        {frontEndLanguages.map(value => <li>{value}</li>)}
-      </ul>
-    </div>
+    <section className="container-stack">
+      <div className='container stack-card'>
+        <div className="row">
+          <Item text="React"/>
+          <Item text="Bootstrap"/>
+          <Item text="Rest APIs"/>
+        </div>
+        <div className="row">
+          <Item text="Redux"/>
+          <Item text="Sass"/>
+          <Item text="Handlebars"/>
+        </div>
+        <div className="row">
+          <Item text="Typescript"/>
+          <Item text="Styled Components"/>
+        </div>
+      </div>
+
+      <div className="d-flex justify-content-between pl-0 mt-5">
+        <h3 className="px-4">stack(pedro.frontend)</h3>
+        <h3 className="px-4">stack(pedro.backend)</h3>
+      </div>
+    </section>
   )
 }
 
