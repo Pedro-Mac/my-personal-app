7565b0b17411d40031f9340e632bfcce0b433bba
KozaX <55134615+KozaX@users.noreply.github.com>
2020-11-02 14:48:09 +0100

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.scss';
+import TitleSection from './components/TitleSection';
+
+function App() {
+  return (
+    <div className="App">
+      <TitleSection />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/TitleSection/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import './styles.scss'
+
+const TitleSection: React.FC = () => {
+  return (
+    <section>
+      <div>
+        <h1 className="text-left" >Hi, I am</h1>
+        <h2 className="text-left" >'Pedro'</h2>
+      </div>
+    </section>
+  )
+}
+
+export default TitleSection;
